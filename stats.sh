eba4ae8f0bd958e023b49b8f1794ebbab7f7392a
Zachery Hysong <zlhsyong@gmail.com>
2020-04-28 16:07:16 -0400

    fix: add full path to log output

--- stats.sh
+++ stats.sh
@@ -17,10 +17,10 @@ vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' # today's t
 printf "$(timestamp) " >> log.txt
 
 # These just output the RAM (in percent), Free Disk Space (in percent),and CPU Load (Lord help me this shit is weird) and outputs it to a file for us to refer to later
-free --mega | grep Mem | awk '{printf "%.2f ", $(NF-4)*100/$(NF-5)}' >> log.txt
-free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-4)}' >> log.txt
-free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-5)}' >> log.txt
-df -h | grep /dev/vda1 | awk '{printf "%.2f ", $(NF-1)}' >> log.txt
-top -bn1 | grep load | awk '{printf "%.2f ", $(NF-2)}' >> log.txt
-vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f ", $(NF-1)}' >> log.txt # today's received
-vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' >> log.txt # today's transmitted
+free --mega | grep Mem | awk '{printf "%.2f ", $(NF-4)*100/$(NF-5)}' >> /home/ubuntu/getStats/log.txt
+free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-4)}' >> /home/ubuntu/getStats/log.txt
+free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-5)}' >> /home/ubuntu/getStats/log.txt
+df -h | grep /dev/vda1 | awk '{printf "%.2f ", $(NF-1)}' >> /home/ubuntu/getStats/log.txt
+top -bn1 | grep load | awk '{printf "%.2f ", $(NF-2)}' >> /home/ubuntu/getStats/log.txt
+vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f ", $(NF-1)}' >> /home/ubuntu/getStats/log.txt # today's received
+vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' >> /home/ubuntu/getStats/log.txt # today's transmitted
