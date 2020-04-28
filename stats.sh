7da25e13fc0f2e7cd7f39d1485eb1575f3560f82
Zachery Hysong <zlhsyong@gmail.com>
2020-04-28 16:08:44 -0400

    fix: add full path to log

--- stats.sh
+++ stats.sh
@@ -14,7 +14,7 @@ top -bn1 | grep load | awk '{printf "%.2f ", $(NF-2)}'
 vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f ", $(NF-1)}' # today's received
 vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' # today's transmitted
 
-printf "$(timestamp) " >> log.txt
+printf "$(timestamp) " >> /home/ubuntu/getStats/log.txt
 
 # These just output the RAM (in percent), Free Disk Space (in percent),and CPU Load (Lord help me this shit is weird) and outputs it to a file for us to refer to later
 free --mega | grep Mem | awk '{printf "%.2f ", $(NF-4)*100/$(NF-5)}' >> /home/ubuntu/getStats/log.txt
