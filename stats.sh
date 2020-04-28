4d66690c0ad029899eb1420919f44f1242d5aabc
Zachery Hysong <zlhsyong@gmail.com>
2020-04-28 12:48:19 -0400

    fix: add proper spaces

--- stats.sh
+++ stats.sh
@@ -10,8 +10,8 @@ free --mega | grep Mem | awk '{printf "%.2f ", $(NF-4)*100/$(NF-5)}'
 free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-4)}'
 free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-5)}'
 df -h | grep rootfs | awk '{printf "%.2f ", $(NF-2)/$(NF-3)}'
-top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}'
-vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f", $(NF-1)}' # today's received
+top -bn1 | grep load | awk '{printf "%.2f ", $(NF-2)}'
+vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f ", $(NF-1)}' # today's received
 vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' # today's transmitted
 
 printf "$(timestamp) " >> log.txt
@@ -21,6 +21,6 @@ free --mega | grep Mem | awk '{printf "%.2f ", $(NF-4)*100/$(NF-5)}' >> log.txt
 free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-4)}' >> log.txt
 free --mega | grep Mem | awk '{printf "%2.0f ", $(NF-5)}' >> log.txt
 df -h | grep rootfs | awk '{printf "%.2f ", $(NF-2)/$(NF-3)}' >> log.txt
-top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}' >> log.txt
-vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f", $(NF-1)}' >> log.txt # today's received
+top -bn1 | grep load | awk '{printf "%.2f ", $(NF-2)}' >> log.txt
+vnstat --oneline | cut -d ';' -f4 | awk '{printf "%.2f ", $(NF-1)}' >> log.txt # today's received
 vnstat --oneline | cut -d ';' -f5 | awk '{printf "%.2f\n", $(NF-1)}' >> log.txt # today's transmitted
